c612e08268c6ac10adbdcda53373ce982cd77ab8
Yang Luo <yangluo86@gmail.com>
2019-07-10 16:45:09 -0400

    added association results with ancestry as covariate

--- scripts/SF5_gwas_assoc_plot.R
+++ scripts/SF5_gwas_assoc_plot.R
@@ -14,3 +14,14 @@ dev.off()
 
 #### common association study stats have been deposit to GWAS Catalog, 
 #### plotting script is similar as above
+
+x <- read.table("data/all.assoc.txt.gz",h=F,stringsAsFactors = F)
+names(x)<-c("CHR","BP","P")
+
+png("figures/SF18a_withancestry-qqplot.png",width=800,height=800)
+qqunif.plot(x$P)
+dev.off()
+
+png("figures/SF18b_withancestry-manhattan.png",width=1000,height=650)
+manhattan(x)
+dev.off()
